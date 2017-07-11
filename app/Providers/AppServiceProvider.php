610309ec0d979af39e62c8ec3c449d16842b37a5
Wiput Pootong <wiput.pootong@gmail.com>
2017-07-11 10:26:45 +0700

    Fix Syntax error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
