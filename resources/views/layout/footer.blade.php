9ad7744302d32b3fa211a341feb7bd34263570ec
Wiput Pootong <wiput.pootong@gmail.com>
2017-07-21 22:25:03 +0700

    Edit footer link

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,11 +3,11 @@
         <div class="footer-left">©2017 Faculty of Information Technology, KMITL</div>
         <div class="footer-right">
 
-            <p><a href="">2012</a> |
-                <a href="">2013</a> |
-                <a href="">2014</a> |
-                <a href="">2016</a> |
-                <a href="">2017 | Developer</a></p>
+            <p><a href="/2012">2012</a> |
+                <a href="/2013">2013</a> |
+                <a href="/2014">2014</a> |
+                <a href="/2016">2016</a> |
+                <a href="/2017">2017 | Developer</a></p>
         </div>
     </div>
 </footer>
