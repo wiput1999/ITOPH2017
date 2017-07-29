8ee8d3a2894e22756958ef6771ccc632a80820aa
Wiput Pootong <wiput.pootong@gmail.com>
2017-07-29 17:23:34 +0700

    Add favicon for dashboard

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ URL::asset('assets/css/backend.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/css/simple-sidebar.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/css/font-awesome.min.css') }}">
+    <link rel="shortcut icon" href="{{URL('/favicon.ico')}}" type="image/x-icon"/>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
