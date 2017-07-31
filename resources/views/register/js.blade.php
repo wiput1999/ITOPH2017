bee12e21af63bd6f6bc26ba6df21fffefcd5c219
Wiput Pootong <wiput.pootong@gmail.com>
2017-08-01 00:44:37 +0700

    Add Google Analytics

--- resources/views/register/js.blade.php
+++ resources/views/register/js.blade.php
@@ -2,3 +2,4 @@
 <script src="{{URL('/assets/js/jquery-3.2.1.min.js')}}"></script>
 <script src="{{URL('/assets/js/bootstrap.min.js')}}"></script>
 <script src="{{URL('/assets/js/owl.carousel.min.js')}}"></script>
+@include('layout.analytics')
