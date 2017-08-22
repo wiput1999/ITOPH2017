8f6e693ccbdc0dac5ba17f41adc91e46f34a95e4
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-08-22 21:12:48 +0700

    Add Approved link

--- resources/views/register/competition/network_rule.blade.php
+++ resources/views/register/competition/network_rule.blade.php
@@ -74,8 +74,8 @@
 
             <hr>
             <div class="text-center">
-                <a href="{{URL('/competition/network/register')}}">
-                    <button type="submit" class="btn btn-primary btn-lg" disabled>ปิดรับสมัคร 🙈</button>
+                <a href="{{URL('/competition/approved/network')}}">
+                    <button class="btn btn-primary btn-lg">รายชื่อผู้มีสิทธิ์แข่งขัน</button>
                 </a>
             </div>
         </div>
