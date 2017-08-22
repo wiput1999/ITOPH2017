a9b42605a131cd6a9e3003d595c0b32e5dd298e3
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-08-22 21:15:34 +0700

    Add Approved link

--- resources/views/register/competition/quiz_rule.blade.php
+++ resources/views/register/competition/quiz_rule.blade.php
@@ -93,8 +93,8 @@
 
             <hr>
             <div class="text-center">
-                <a href="{{URL('/competition/quiz/register')}}">
-                    <button type="submit" class="btn btn-primary btn-lg" disabled>ปิดรับสมัคร 🙈</button>
+                <a href="{{URL('/competition/approved/quiz')}}">
+                    <button class="btn btn-primary btn-lg">รายชื่อผู้มีสิทธ์แข่งขัน</button>
                 </a>
             </div>
         </div>
